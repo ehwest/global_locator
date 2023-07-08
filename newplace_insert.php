888d7d0108497cd1b2989bfe0f3461da3f85e11b
hamzashahab1610 <hamzashahab1610@gmail.com>
2023-07-08 12:05:46 +0500

    Prepared statements

--- newplace_insert.php
+++ newplace_insert.php
@@ -1,8 +1,10 @@
 <?php
+$data = json_decode($_REQUEST["data"]);
+$address_compponents = $data->address_components;
+
 $realname = $_REQUEST["realname"];
 $youremail = $_REQUEST["youremail"];
 $country = $_REQUEST["country"];
-$county = $_REQUEST["county"];
 $lat = $_REQUEST["latitude"];
 $lng = $_REQUEST["longitude"];
 $multi_campus = $_REQUEST["multi_campus"];
@@ -27,6 +29,18 @@ $s33 = $_REQUEST["homepage"];
 $s34 = $_REQUEST["hours"];
 $s35 = $_REQUEST["class"];
 
+if (isset($address_components)) {
+  foreach ($address_components as $component) {
+    if (in_array("administrative_area_level_2", $component->types)) {
+      $adminArea = $component->long_name || $component->short_name || "";
+      if ($adminArea) {
+        $county .= ", " . $adminArea;
+      }
+    }
+  }
+}
+
+echo "data: " . $data . "<br>";
 echo "realname: " . $realname . "<br>";
 echo "youremail: " . $youremail . "<br>";
 echo "latitude: " . $lat . "<br>";
@@ -34,7 +48,7 @@ echo "longitude: " . $lng . "<br>";
 echo "name: " . $s10 . "<br>";
 echo "street1: " . $s11 . "<br>";
 echo "street2: " . $s12 . "<br>";
-echo "ccity: " .  $s13 . "<br>";
+echo "ccity: " . $s13 . "<br>";
 echo "state: " . $s14 . "<br>";
 echo "zipcode: " . $s15 . "<br>";
 echo "country: " . $country . "<br>";
@@ -53,55 +67,89 @@ echo "hours: " . $s34 . "<br>";
 echo "character: " . $s23 . "<br>";
 echo "class: " . $s35 . "<br>";
 
-$q = "INSERT INTO pending_registry SET ";
-$q .= " fname='" . $s10 . "', ";
-$q .= " addressline1='" . $s11 . "', ";
-$q .= " addressline2='" . $s12 . "', ";
-$q .= " addresscity='" . $s13 . "', ";
-$q .= " addressstate='" . $s14 . "', ";
-$q .= " addresspostalcode='" . $s15 . "', ";
-$q .= " addresscounty='" . addslashes(trim($county)) . "', ";
-$q .= " addresscountrycode='" . $s17 . "', ";
-$q .= " size='" . $s20 . "', ";
-$q .= " url='" . $s33 . "', ";
-$q .= " email='" . $s32 . "', ";
-$q .= " phone='" . $s31 . "', ";
-$q .= " contact='" . $s30 . "', ";
-$q .= " qa1='" . $s42 . "', ";
-$q .= " qa2='" . $s34 . "', ";
-$q .= " qa3='" . $s21 . "', ";
-$q .= " qa4='" . $s22 . "', ";
-$q .= " providername='" . $realname . "', ";
-$q .= " provideremail='" . $youremail . "', ";
-$q .= " instrumental='" . $instrumental . "', ";
-$q .= " both_i_a='" . $both_i_a . "', ";
-$q .= " multi_campus='" . $multi_campus . "', ";
-$q .= " egalitarian='" . $egalitarian . "', ";
-$q .= " flattitude='" . $lat . "', ";
-//$q .= " lat='" . $lat . "', ";
-$q .= " flongitude='" . $lng . "', ";
-//$q .= " lng='" . $lng . "', ";
-$nowtime = time();
-$q .= " geocoded='" . $nowtime . "', ";
-$q .= " class='" . $s35 . "', ";
-$q .= " modified='" . $s43 . "', ";
-$q .= " ipaddress='" . $ip . "', ";
-$q .= " uid='" . $s44 . "'; ";
+$sql = "INSERT INTO pending_registry
+          (
+            fname, 
+            addressline1,
+            addressline2,
+            addresscity,
+            addressstate,
+            addresspostalcode,
+            addresscounty,
+            addresscountrycode,
+            size,
+            url,
+            email,
+            phone,
+            contact,
+            qa1,
+            qa2,
+            qa3,
+            qa4,
+            providername,
+            provideremail,
+            instrumental,
+            both_i_a,
+            multi_campus,
+            egalitarian,
+            flattitude,
+            flongitude,
+            geocoded,
+            class,
+            modified,
+            ipaddress,
+            uid
+          ) 
+          VALUES (?, ?)";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(1, $s10);
+$stmt->bindParam(2, $s11);
+$stmt->bindParam(3, $s12);
+$stmt->bindParam(4, $s13);
+$stmt->bindParam(5, $s14);
+$stmt->bindParam(6, $s15);
+$stmt->bindParam(7, addslashes(trim($county)));
+$stmt->bindParam(8, $s17);
+$stmt->bindParam(9, $s20);
+$stmt->bindParam(10, $s33);
+$stmt->bindParam(11, $s32);
+$stmt->bindParam(12, $s31);
+$stmt->bindParam(13, $s30);
+$stmt->bindParam(14, $s42);
+$stmt->bindParam(15, $s34);
+$stmt->bindParam(16, $s21);
+$stmt->bindParam(17, $s22);
+$stmt->bindParam(18, $realname);
+$stmt->bindParam(19, $youremail);
+$stmt->bindParam(20, $instrumental);
+$stmt->bindParam(21, $both_i_a);
+$stmt->bindParam(22, $multi_campus);
+$stmt->bindParam(23, $egalitarian);
+$stmt->bindParam(24, $lat);
+$stmt->bindParam(25, $lng);
+$stmt->bindParam(26, time());
+$stmt->bindParam(27, $s35);
+$stmt->bindParam(28, $s43);
+$stmt->bindParam(29, $ip);
+$stmt->bindParam(30, $s44);
+
+$stmt->execute();
+
+if ($stmt->rowCount() > 0) {
+  echo "Record inserted successfully.";
+} else {
+  echo "Error inserting record.";
+}
 
 function validated_searchname($input)
 {
-  //$filteredInput = preg_replace('/[^a-zA-Z0-9]/', '', $input);
-  $filteredInput   = preg_replace('/[^a-zA-Z0-9\s]/', '', $input); //allows white space in the input
+  $filteredInput = preg_replace('/[^a-zA-Z0-9\s]/', '', $input);
   if ($filteredInput == $input) {
-    //print " Valid searchname input";
-    return ($input);
+    return $input;
   } else {
     print "Unsafe SQL insertion detected.  ";
-    //print " Bad (decoded) value is:  ". $input;
-    //print " Debug raw parameter= " . $input;
-    //print " Debug=Forgot to base64_encode?  Try this value: " . base64_encode( base64_decode($input));
-    //print  " Invalidated association input";
-    //print  make_json("Invalidated association input");
     exit();
   }
 }
