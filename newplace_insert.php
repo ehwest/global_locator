d1f9ddd95692e3654a0d806a72fd9216e38fb635
hamzashahab1610 <hamzashahab1610@gmail.com>
2023-07-11 20:56:47 +0500

    new updates

--- newplace_insert.php
+++ newplace_insert.php
@@ -1,6 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
 require_once("config.php");
 
+$ip = trim($_SERVER['REMOTE_ADDR']);
 $data = json_decode($_REQUEST["data"]);
 $address_compponents = $data->address_components;
 
@@ -31,10 +35,9 @@ $s33 = $_REQUEST["homepage"];
 $s34 = $_REQUEST["hours"];
 $s35 = $_REQUEST["class"];
 
-// formatted_address extraction
-$formatted_address = $data->formatted_address;
-
+$address_components = $_REQUEST["adress_components"];
 if (isset($address_components)) {
+  //print_r($address_components);
   foreach ($address_components as $component) {
     if (in_array("administrative_area_level_2", $component->types)) {
       $adminArea = $component->long_name || $component->short_name || "";
@@ -42,7 +45,6 @@ if (isset($address_components)) {
         $county .= ", " . $adminArea;
       }
     }
-
     // extract the country code
     if (in_array("country", $component->types)) {
       $countryCode = $component->short_name || "";
@@ -53,153 +55,124 @@ if (isset($address_components)) {
   }
 }
 
-// YOUR WORK (APPENDING TO QUERY)
-$q = "INSERT INTO pending_registry SET ";
-$q .= " fname='" . $s10 . "', ";
-$q .= " addressline1='" . $s11 . "', ";
-$q .= " addressline2='" . $s12 . "', ";
-$q .= " addresscity='" . $s13 . "', ";
-$q .= " addressstate='" . $s14 . "', ";
-$q .= " addresspostalcode='" . $s15 . "', ";
-$q .= " addresscounty='" . addslashes(trim($county)) . "', ";
-$q .= " addresscountrycode='" . $s17 . "', ";
-$q .= " size='" . $s20 . "', ";
-$q .= " url='" . $s33 . "', ";
-$q .= " email='" . $s32 . "', ";
-$q .= " phone='" . $s31 . "', ";
-$q .= " contact='" . $s30 . "', ";
-$q .= " qa1='" . $s42 . "', ";
-$q .= " qa2='" . $s34 . "', ";
-$q .= " qa3='" . $s21 . "', ";
-$q .= " qa4='" . $s22 . "', ";
+//ok this works
+$qtry = "INSERT INTO pending_registry (
+fname, 
+addressline1,
+addressline2,
+addresscity,
+addressstate, 
+addresscounty,
+addresscountrycode,
+countryname,
+size,
+url,
+email,
+phone,
+contact,
+qa1,
+qa2,
+qa3,
+qa4,
+providername,
+provideremail,
+instrumental,
+both_i_a,
+egalitarian,
+multi_campus,
+flattitude,
+flongitude,
+class,
+modified,
+ipaddress,
+formatted_address
+) 
+VALUES (
+?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+)";
 
-$q .= " providername='" . $_REQUEST["realname"] . "', ";
-$q .= " provideremail='" . $_REQUEST["youremail"] . "', ";
+$stmt = $dbLink->prepare($qtry);
+$stmt->bind_param(
 
-$q .= " instrumental='" . $instrumental . "', ";
-$q .= " both_i_a='" . $both_i_a . "', ";
-$q .= " multi_campus='" . $multi_campus . "', ";
-$q .= " egalitarian='" . $egalitarian . "', ";
+  "sssssssssssssssssssssssddsiss",
 
-$q .= " flattitude='" . $lat . "', ";
-$q .= " flongitude='" . $lng . "', ";
-$nowtime = time();
-$q .= " geocoded='" . $nowtime . "', ";
-//$q .= " class='" . $s35 . "', ";
-$q .= " class='" .    $nowtime . "', ";
-$q .= " modified='" . $nowtime . "', ";
-$q .= " ipaddress='" . $ip . "', ";
-$q .= " uid='" . 1 . "'; ";
-
-
-//        all this is in config.php on line 2
-//        if( !($dbLink = mysqli_connect($dbhostname, $dblogin, $dbpassword))) {
-//                 print("Failed phase1 to connect to server 1!\n");
-//                 print("Request Aborted!\n");
-//                 exit();
-//        }
-
-//        if( ! mysqli_select_db($dbLink, $dbname) ) {
-//                 print("Failed phase2 to connect to database on server2!<BR>\n");
-//                 print("Request Aborted!\n");
-//                 exit();
-//        }
-
-// if ($result = mysqli_query($dbLink, $q)) {
-//   $numRows = mysqli_affected_rows($dbLink);
-//   // Return the number of rows in result set
-//   //printf("Result set has %d rows.\n",$numRows);
-//   //print "INSERTED RECORD SUCCESSFULLY";
-// } else {
-//   print "failed to insert for some reason";
-// }
-// print "$numRows WAS successfully inserted, thanks!";
-// // Redirect the user to another page
-// header("Location: thanks.html");
-// exit();
-
-// MY WORK (PREPARED SQL STATEMENT USING mysqli_prepare AND mysqli_stmt_bind_param FUNCTIONS)
-$sql = "INSERT INTO pending_registry
-          (
-            fname, 
-            addressline1,
-            addressline2,
-            addresscity,
-            addressstate,
-            addresspostalcode,
-            addresscounty,
-            addresscountrycode,
-            size,
-            url,
-            email,
-            phone,
-            contact,
-            qa1,
-            qa2,
-            qa3,
-            qa4,
-            providername,
-            provideremail,
-            instrumental,
-            both_i_a,
-            multi_campus,
-            egalitarian,
-            flattitude,
-            flongitude,
-            geocoded,
-            class,
-            modified,
-            ipaddress,
-            uid,
-            formatted_address
-          ) 
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-$stmt = mysqli_prepare($dbLink, $sql);
-
-mysqli_stmt_bind_param(
-  $stmt,
-  "sssssssssssssssssssddssisis",
-  $s10,
-  $s11,
-  $s12,
-  $s13,
-  $s14,
-  $s15,
-  addslashes(trim($county)),
-  $s17,
-  $s20,
-  $s33,
-  $s32,
-  $s31,
-  $s30,
-  $s42,
-  $s34,
-  $s21,
-  $s22,
-  $realname,
-  $youremail,
+  $fname,
+  $addressline1,
+  $addressline2,
+  $addresscity,
+  $addressstate,
+  $addresscounty,
+  $addresscountrycode,
+  $countryname,
+  $size,
+  $url,
+  $email,
+  $phone,
+  $contact,
+  $qa1,
+  $qa2,
+  $qa3,
+  $qa4,
+  $providername,
+  $provideremail,
   $instrumental,
   $both_i_a,
-  $multi_campus,
   $egalitarian,
-  $lat,
-  $lng,
-  time(),
-  $s35,
-  $s43,
-  $ip,
-  $s44,
-  $formatted_address,
+  $multi_campus,
+  $flattitude,
+  $flongitude,
+  $class,
+  $modified,
+  $ipaddress,
+  $formatted_address
 );
 
-mysqli_stmt_execute($stmt);
-$dbResult = mysqli_stmt_get_result($stmt);
-echo "dbResult: " . $dbResult . "<br>";
+$fname         = $s10;
+$addressline1  = $s11;
+$addressline2  = $s12;
+$addresscity   = $s13;
+$addressstate  = $s14;
+$addresscounty     = $county;
+$addresscountrycode = $s17;
+$countryname      = $s16;
+$size        = $s20;
+$url        = $s33;
+$email        = $s32;
+$phone        = $s31;
+$contact      = $s30;
+$qa1        = $s42;
+$qa2        = $s34;
+$qa3        = $s21;
+$qa4        = $s22;
+$providername      = $_REQUEST["realname"];
+$provideremail      = $_REQUEST["youremail"];
+$instrumental       = $_REQUEST["instrumental"];
+$both_i_a      = $_REQUEST["both_i_a"];
+$egalitarian      = $_REQUEST["egalitarian"];
+$multi_campus      = $_REQUEST["multi_campus"];
+$flattitude      = $_REQUEST["flattitude"];
+$flongitude      = $_REQUEST["flongitude"];
+$class        = $s35;
+$modified      = $nowtime;
+$ipaddress      = $ip;
+$formatted_addresss = $data->formatted_address;
 
-// header("Location: thanks.html");
-echo '<script>window.open("thanks.html", "_blank");</script>';
-exit();
+$stmt->execute();
+$result = $stmt->get_result();
+
+//print_r( $result );;
+//print_r( $stmt);;
+$numrows = $stmt->affected_rows;
+//print "numrows=$numrows ";
+print $numrows;
+
+if ($numrows > 0) {
+  header("Location: thanks.html?message=");
+  exit();
+} else {
+  header("Location: error.html?message=");
+  exit();
+}
 
 function validated_searchname($input)
 {
@@ -207,7 +180,7 @@ function validated_searchname($input)
   if ($filteredInput == $input) {
     return $input;
   } else {
-    print "Unsafe SQL insertion detected.  ";
+    print "Unsafe SQL insertion detected. ";
     exit();
   }
 }
