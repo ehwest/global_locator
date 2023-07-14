8b6791507044e0a347d64b6223c4151df1d103b9
hamzashahab1610 <hamzashahab1610@gmail.com>
2023-07-14 18:44:18 +0500

    new changes

--- newplace_insert.php
+++ newplace_insert.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once("config.php");
 
-$ip = trim($_SERVER['REMOTE_ADDR'] );
+$ip = trim($_SERVER['REMOTE_ADDR']);
 $data = json_decode($_REQUEST["data"]);
 $address_compponents = $data->address_components;
 
@@ -27,6 +27,7 @@ $s12 = $_REQUEST["street2"];
 $s13 = $_REQUEST["ccity"];
 $s14 = $_REQUEST["state"];
 $s15 = $_REQUEST["zipcode"];
+$s17 = $_REQUEST["iso3166"];
 $s20 = $_REQUEST["members"];
 $s21 = $_REQUEST["elders"];
 $s22 = $_REQUEST["ministerstatus"];
@@ -37,19 +38,28 @@ $s32 = $_REQUEST["email"];
 $s33 = $_REQUEST["homepage"];
 $s34 = $_REQUEST["hours"];
 $s35 = $_REQUEST["class"];
+$county = $_REQUEST["addresscounty"];
 
 $address_components = $_REQUEST["adress_components"];
-if (isset($address_components)) {
-//print_r($address_components);
-foreach ($address_components as $component) {
-if (in_array("administrative_area_level_2", $component->types)) {
-$adminArea = $component->long_name || $component->short_name || "";
-if ($adminArea) {
-$county .= ", " . $adminArea;
-}
-}
-}
-}
+
+// if (isset($address_components)) {
+//   //print_r($address_components);
+//   foreach ($address_components as $component) {
+//     if (in_array("administrative_area_level_2", $component->types)) {
+//       $adminArea = $component->long_name || $component->short_name || "";
+//       if ($adminArea) {
+//         $county .= ", " . $adminArea;
+//       }
+//     }
+//     // extract the country code
+//     if (in_array("country", $component->types)) {
+//       $countryCode = $component->short_name || "";
+//       if ($countryCode) {
+//         $s17 = $countryCode;
+//       }
+//     }
+//   }
+// }
 
 
 //ok this works
@@ -91,37 +101,37 @@ VALUES (
 $stmt = $dbLink->prepare($qtry);
 $stmt->bind_param(
 
-"sssssssssssssssssssssssddsiss",
+  "sssssssssssssssssssssssddsiss",
 
-$fname, 
-$addressline1,
-$addressline2,
-$addresscity,
-$addressstate,
-$addresscounty,
-$addresscountrycode,
-$countryname,
-$size,
-$url,
-$email,
-$phone,
-$contact,
-$qa1,
-$qa2,
-$qa3,
-$qa4,
-$providername,
-$provideremail,
-$instrumental,
-$both_i_a,
-$egalitarian,
-$multi_campus,
-$flattitude,
-$flongitude,
-$class,
-$modified,
-$ipaddress,
-$formatted_address
+  $fname,
+  $addressline1,
+  $addressline2,
+  $addresscity,
+  $addressstate,
+  $addresscounty,
+  $addresscountrycode,
+  $countryname,
+  $size,
+  $url,
+  $email,
+  $phone,
+  $contact,
+  $qa1,
+  $qa2,
+  $qa3,
+  $qa4,
+  $providername,
+  $provideremail,
+  $instrumental,
+  $both_i_a,
+  $egalitarian,
+  $multi_campus,
+  $flattitude,
+  $flongitude,
+  $class,
+  $modified,
+  $ipaddress,
+  $formatted_address
 );
 
 $fname         = $s10;
@@ -131,28 +141,28 @@ $addresscity   = $s13;
 $addressstate  = $s14;
 $addresscounty     = $county;
 $addresscountrycode = $s17;
-$countryname	    = $s16;
-$size		    = $s20;
-$url		    = $s33;
-$email		    = $s32;
-$phone		    = $s31;
-$contact	    = $s30;
-$qa1		    = $s42;
-$qa2		    = $s34;
-$qa3		    = $s21;
-$qa4		    = $s22;
-$providername	    = $_REQUEST["realname"];
-$provideremail	    = $_REQUEST["youremail"];
+$countryname      = $s16;
+$size        = $s20;
+$url        = $s33;
+$email        = $s32;
+$phone        = $s31;
+$contact      = $s30;
+$qa1        = $s42;
+$qa2        = $s34;
+$qa3        = $s21;
+$qa4        = $s22;
+$providername      = $_REQUEST["realname"];
+$provideremail      = $_REQUEST["youremail"];
 $instrumental       = $_REQUEST["instrumental"];
-$both_i_a	    = $_REQUEST["both_i_a"];
-$egalitarian	    = $_REQUEST["egalitarian"];
-$multi_campus	    = $_REQUEST["multi_campus"];
-$flattitude	    = $_REQUEST["flattitude"];
-$flongitude	    = $_REQUEST["flongitude"];
-$class		    = $s35;
-$modified	    = $nowtime;
-$ipaddress	    = $ip;
-$formatted_addresss = $_REQUEST['formatted_address'];
+$both_i_a      = $_REQUEST["both_i_a"];
+$egalitarian      = $_REQUEST["egalitarian"];
+$multi_campus      = $_REQUEST["multi_campus"];
+$flattitude      = $_REQUEST["flattitude"];
+$flongitude      = $_REQUEST["flongitude"];
+$class        = $s35;
+$modified      = $nowtime;
+$ipaddress      = $ip;
+$formatted_addresss = $data->formatted_address;
 
 $stmt->execute();
 $result = $stmt->get_result();
@@ -174,11 +184,11 @@ exit();
 
 function validated_searchname($input)
 {
-$filteredInput = preg_replace('/[^a-zA-Z0-9\s]/', '', $input);
-if ($filteredInput == $input) {
-return $input;
-} else {
-print "Unsafe SQL insertion detected. ";
-exit();
-}
+  $filteredInput = preg_replace('/[^a-zA-Z0-9\s]/', '', $input);
+  if ($filteredInput == $input) {
+    return $input;
+  } else {
+    print "Unsafe SQL insertion detected. ";
+    exit();
+  }
 }
